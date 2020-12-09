1f41d3eb598bed58692d8c7ab5882eb3541fe660
Sam Baumann <sbauman2@vols.utk.edu>
2020-12-09 17:25:02 -0600

    Handle blog posts

--- contenthandler.js
+++ contenthandler.js
@@ -1,13 +1,10 @@
 
-let div = document.createElement('div');
-let internals = document.createElement('div');
-let img = document.createElement('img');
-let p = document.createElement('p');
 let elem = document.getElementById('contentcontainer');
 //open the blogposts.json file
 //this seems like a mess, but I can't find a better way to do this
 var rawFile = new XMLHttpRequest();
-rawFile.open("GET", "./blogposts.json", false);
+var data;
+rawFile.open("GET", "https://raw.githubusercontent.com/sam-baumann/sam-baumann.github.io/main/blogposts.json", false);
 rawFile.onreadystatechange = function ()
 {
     if(rawFile.readyState === 4)
@@ -15,20 +12,33 @@ rawFile.onreadystatechange = function ()
         if(rawFile.status === 200 || rawFile.status == 0)
         {
             var allText = rawFile.responseText;
-            var data = JSON.parse(allText);
+            data = JSON.parse(allText);
         }
     }
 }
 rawFile.send(null);
 
-console.log(data.image);
-div.className = "content";
-div.innerHTML = "<h1>Content example inserted by external JS</h1>"
-internals.className = "internalcontent";
-img.src = "images/flash01.jpeg";
-p.innerHTML = "This is example text";
-internals.append(img);
-internals.append(p);
+let j = 0;
+if (data.content.length - 10 > j) j = data.content.length - 10;
 
-div.append(internals);
-elem.append(div);
+var i;
+for (i = data.content.length - 1; i >= j; i--){
+    let div = document.createElement('div');
+    let internals = document.createElement('div');
+    let img = document.createElement('img');
+    let p = document.createElement('p');
+
+    div.className = "content";
+    internals.className = "internalcontent";
+
+    div.innerHTML = "<h1>" + data.content[i].heading + "</h1>";
+    
+    img.src = data.content[i].image;
+    p.innerHTML = data.content[i].text;
+
+    internals.append(img);
+    internals.append(p);
+
+    div.append(internals);
+    elem.append(div);
+}
