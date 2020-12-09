f84c86f1741b6a8126d9cb7dd82d6cbb76e6347b
Sam Baumann <sbauman2@vols.utk.edu>
2020-12-09 17:01:19 -0600

    test of request

--- contenthandler.js
+++ contenthandler.js
@@ -1,7 +1,34 @@
 
 let div = document.createElement('div');
+let internals = document.createElement('div');
+let img = document.createElement('img');
+let p = document.createElement('p');
 let elem = document.getElementById('contentcontainer');
+//open the blogposts.json file
+//this seems like a mess, but I can't find a better way to do this
+var rawFile = new XMLHttpRequest();
+rawFile.open("GET", "./blogposts.json", false);
+rawFile.onreadystatechange = function ()
+{
+    if(rawFile.readyState === 4)
+    {
+        if(rawFile.status === 200 || rawFile.status == 0)
+        {
+            var allText = rawFile.responseText;
+            var data = JSON.parse(allText);
+        }
+    }
+}
+rawFile.send(null);
+
+console.log(data.image);
 div.className = "content";
 div.innerHTML = "<h1>Content example inserted by external JS</h1>"
+internals.className = "internalcontent";
+img.src = "images/flash01.jpeg";
+p.innerHTML = "This is example text";
+internals.append(img);
+internals.append(p);
 
+div.append(internals);
 elem.append(div);
