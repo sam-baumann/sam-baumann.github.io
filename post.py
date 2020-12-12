dd19b6d21c38ca694327e5a1ff9e3c68aead49de
Sam Baumann <sbauman2@vols.utk.edu>
2020-12-11 21:12:17 -0600

    Upeated python code

--- post.py
+++ post.py
@@ -10,7 +10,13 @@ newpost = {}
 #posts need image, heading, text, and date
 newpost["image"] = input("Enter the image link: ")
 newpost["heading"] = input("Enter the post title: ")
-newpost["text"] = input("Enter the text for your post: ")
+inputstring = input("Use existing text file for post? [y]/n")
+if inputstring == "n":
+    newpost["text"] = input("Enter the text for your post: ")
+else:
+    textfilename = input("Enter the filename: ")
+    textfile = open(textfilename, "r")
+    newpost["text"] = textfile.read()
 newpost["Date"] = input("Enter the date for the post: ")
 
 j["content"].append(newpost)
